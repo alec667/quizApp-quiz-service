37788d3524c4c94f050a8ffe4a7e6083fde3776b
Alejandro Casiva <alec667@gmail.com>
2023-12-11 20:16:52 -0300

    More controller methods added

--- src/main/java/com/example/quizservice/controller/QuizController.java
+++ src/main/java/com/example/quizservice/controller/QuizController.java
@@ -1,14 +1,14 @@
 package com.example.quizservice.controller;
 
 import com.example.quizservice.model.Quiz;
+import com.example.quizservice.model.QuizAnswers;
 import com.example.quizservice.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/quiz")
@@ -17,9 +17,19 @@ public class QuizController {
     @Autowired
     QuizService quizService;
 
-    @GetMapping(path = "create")
-    public ResponseEntity<String> createQuiz(@RequestBody Quiz quiz){
+    @PostMapping(path = "create")
+    public ResponseEntity<String> createQuiz(@RequestBody Quiz quiz) {
         return new ResponseEntity<>(quizService.createQuiz(quiz), HttpStatus.CREATED);
+    }
+
+    @GetMapping(path = "{quizId}")
+    public ResponseEntity<Quiz> getQuiz(@PathVariable("quizId") Integer quizId) {
+        return new ResponseEntity<>(quizService.getQuiz(quizId), HttpStatus.OK);
+    }
+
+    @PostMapping("submit/{quizId}")
+    public ResponseEntity<Integer> submitAnswers(@PathVariable("quizId") Integer quizId, @RequestBody List<QuizAnswers> answersList) {
+        return new ResponseEntity<>(quizService.calculateResult(quizId, answersList), HttpStatus.OK);
     }
 
 
