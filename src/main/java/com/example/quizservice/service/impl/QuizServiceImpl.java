1cac3640e28fb9349b47a8b372d73f2c3fa7af30
Alejandro Casiva <alec667@gmail.com>
2023-12-15 22:44:39 -0300

    feat(QuizService): add question for quiz method

--- src/main/java/com/example/quizservice/service/impl/QuizServiceImpl.java
+++ src/main/java/com/example/quizservice/service/impl/QuizServiceImpl.java
@@ -1,12 +1,14 @@
 package com.example.quizservice.service.impl;
 
 import com.example.quizservice.exception.QuizNotFoundException;
+import com.example.quizservice.feign.QuizInterface;
 import com.example.quizservice.model.QuestionWrapper;
 import com.example.quizservice.model.Quiz;
 import com.example.quizservice.model.QuizAnswers;
 import com.example.quizservice.model.QuizDataTransferObject;
 import com.example.quizservice.repository.QuizRepository;
 import com.example.quizservice.service.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,6 +16,9 @@ import java.util.Optional;
 
 @Service
 public class QuizServiceImpl implements QuizService {
+
+    @Autowired
+    private QuizInterface quizInterface;
 
     final QuizRepository quizRepository;
 
@@ -23,10 +28,10 @@ public class QuizServiceImpl implements QuizService {
 
     @Override
     public Quiz getQuiz(Integer quizId) {
-        Optional<Quiz> optionalQuiz= quizRepository.findById(quizId);
-        if (optionalQuiz.isPresent()){
+        Optional<Quiz> optionalQuiz = quizRepository.findById(quizId);
+        if (optionalQuiz.isPresent()) {
             return optionalQuiz.get();
-        }else {
+        } else {
             throw new QuizNotFoundException("Quiz not found :(");
 
         }
@@ -34,13 +39,14 @@ public class QuizServiceImpl implements QuizService {
 
     @Override
     public String createQuiz(QuizDataTransferObject quizDTO) {
-
-
+        List<Integer> questionsIds = quizInterface.getQuestionsForQuiz(quizDTO.getCategory(), quizDTO.getNumOfQuestions()).getBody();
 
         Quiz quiz = new Quiz();
         quiz.setTitle(quizDTO.getTitle());
+        quiz.setQuestionsIds(questionsIds);
+        quizRepository.save(quiz);
 
-        return "";
+        return "Quiz created";
 
     }
 
@@ -57,13 +63,14 @@ public class QuizServiceImpl implements QuizService {
 
     @Override
     public List<QuestionWrapper> getQuestionForQuiz(Integer id) {
-        Optional<Quiz>  optionalQuiz = quizRepository.findById(id);
-        if (optionalQuiz.isPresent()){
-            //TODO complete this
-
+        Optional<Quiz> optionalQuiz = quizRepository.findById(id);
+        List<QuestionWrapper> questionsForUser;
+        if (optionalQuiz.isPresent()) {
+            questionsForUser = quizInterface.getQuestionsFromId(optionalQuiz.get().getQuestionsIds()).getBody();
+            return questionsForUser;
+        }else {
+            throw new QuizNotFoundException("Quiz not found");
         }
-
-        return null;
     }
 
     @Override
