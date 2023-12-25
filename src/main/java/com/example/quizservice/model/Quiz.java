9aa276afa127c5d03f50aa7c53b2ab2b16534297
Alejandro Casiva <alec667@gmail.com>
2023-12-24 23:20:37 -0300

    test(QuizController): add getQuiz method test

--- src/main/java/com/example/quizservice/model/Quiz.java
+++ src/main/java/com/example/quizservice/model/Quiz.java
@@ -1,11 +1,15 @@
 package com.example.quizservice.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Quiz {
 
--- src/test/java/com/example/quizservice/controller/QuizControllerTest.java
+++ src/test/java/com/example/quizservice/controller/QuizControllerTest.java
@@ -1,5 +1,7 @@
 package com.example.quizservice.controller;
 
+import com.example.quizservice.model.QuestionWrapper;
+import com.example.quizservice.model.Quiz;
 import com.example.quizservice.model.QuizDataTransferObject;
 import com.example.quizservice.service.QuizService;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -17,6 +19,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -39,12 +44,19 @@ class QuizControllerTest {
     ObjectMapper objectMapper = new ObjectMapper();
     ObjectWriter objectWriter = objectMapper.writer();
     QuizDataTransferObject quizDTO;
+    Quiz testQuiz1;
+    QuestionWrapper questionWrapper1, questionWrapper2;
+    List<Integer> questionsIds;
 
 
     @BeforeEach
     void setUp() {
         autoCloseable = MockitoAnnotations.openMocks(this);
         this.mockMvc = MockMvcBuilders.standaloneSetup(quizController).build();
+        questionWrapper1 = new QuestionWrapper(1, "What is the default value of float variable?", "0.0d", "0.0f", "0", "not defined");
+        questionWrapper2 = new QuestionWrapper(2, "Which of the following is NOT a keyword in java?", "static", "Boolean", "void", "private");
+        questionsIds = Arrays.asList(questionWrapper1.getId(), questionWrapper2.getId());
+        testQuiz1 = new Quiz(1, "Quiz 1", questionsIds);
 
     }
 
@@ -56,7 +68,8 @@ class QuizControllerTest {
     @Test
     void createQuiz() throws Exception {
         quizDTO = new QuizDataTransferObject("Quiz 1", "Java", 1);
-        String content =objectWriter.writeValueAsString(quizDTO);
+        String content = objectWriter.writeValueAsString(quizDTO);
+
         when(quizService.createQuiz(quizDTO)).thenReturn("Quiz created");
 
         MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
@@ -73,7 +86,17 @@ class QuizControllerTest {
     }
 
     @Test
-    void getQuiz() {
+    void getQuiz() throws Exception {
+        when(quizService.getQuiz(1)).thenReturn(testQuiz1);
+
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
+                .get("/quiz/1")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(mockRequest)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", notNullValue()))
+                .andExpect(jsonPath("$.title", is("Quiz 1")));
     }
 
     @Test
