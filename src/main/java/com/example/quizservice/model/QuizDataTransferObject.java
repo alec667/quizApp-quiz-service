d5c41655d6ff03b8521138ae757440a7fb7cabc6
Alejandro Casiva <alec667@gmail.com>
2023-12-24 23:08:21 -0300

    test(QuizController): add create method test

--- src/main/java/com/example/quizservice/model/QuizDataTransferObject.java
+++ src/main/java/com/example/quizservice/model/QuizDataTransferObject.java
@@ -1,8 +1,10 @@
 package com.example.quizservice.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class QuizDataTransferObject {
 
     private String title;
--- src/test/java/com/example/quizservice/controller/QuizControllerTest.java
+++ src/test/java/com/example/quizservice/controller/QuizControllerTest.java
@@ -1,6 +1,9 @@
 package com.example.quizservice.controller;
 
+import com.example.quizservice.model.QuizDataTransferObject;
 import com.example.quizservice.service.QuizService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,10 +12,17 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(QuizController.class)
 class QuizControllerTest {
@@ -26,19 +36,40 @@ class QuizControllerTest {
     private QuizController quizController;
 
     AutoCloseable autoCloseable;
+    ObjectMapper objectMapper = new ObjectMapper();
+    ObjectWriter objectWriter = objectMapper.writer();
+    QuizDataTransferObject quizDTO;
+
 
     @BeforeEach
     void setUp() {
         autoCloseable = MockitoAnnotations.openMocks(this);
         this.mockMvc = MockMvcBuilders.standaloneSetup(quizController).build();
+
     }
 
     @AfterEach
-    void tearDown() {
+    void tearDown() throws Exception {
+        autoCloseable.close();
     }
 
     @Test
-    void createQuiz() {
+    void createQuiz() throws Exception {
+        quizDTO = new QuizDataTransferObject("Quiz 1", "Java", 1);
+        String content =objectWriter.writeValueAsString(quizDTO);
+        when(quizService.createQuiz(quizDTO)).thenReturn("Quiz created");
+
+        MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders
+                .post("/quiz/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content);
+
+        mockMvc.perform(mockRequest)
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$", notNullValue()))
+                .andExpect(result ->
+                        assertThat(result.getResponse().getContentAsString())
+                                .isEqualTo("Quiz created"));
     }
 
     @Test
