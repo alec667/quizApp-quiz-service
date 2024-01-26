421c4ebe5c3cca10d2555d8208ef764b40b06eca
Alejandro Casiva <alec667@gmail.com>
2024-01-26 00:09:19 -0300

    refactor(QuizController): change mapping consumes and produces properties for controller endpoints

--- src/main/java/com/example/quizservice/controller/QuizController.java
+++ src/main/java/com/example/quizservice/controller/QuizController.java
@@ -21,33 +21,33 @@ public class QuizController {
     @Autowired
     QuizService quizService;
 
-    @PostMapping(path = "create")
+    @PostMapping(path = "create", consumes = "application/json")
     @Operation(summary = "Creates a quiz", description = "creates a quiz and saves it to database", responses = {@ApiResponse(responseCode = "201", description = "status CREATED")})
     public ResponseEntity<String> createQuiz(@RequestBody QuizDataTransferObject quizDTO) {
         return new ResponseEntity<>(quizService.createQuiz(quizDTO), HttpStatus.CREATED);
     }
 
-    @GetMapping(path = "{quizId}")
+    @GetMapping(path = "{quizId}", produces = "application/json")
     @Operation(summary = "Returns a quiz by ID", description = "Provides quiz details", responses = {@ApiResponse(responseCode = "200", description = "status OK")})
     public ResponseEntity<Quiz> getQuiz(@PathVariable("quizId") Integer quizId) {
         return new ResponseEntity<>(quizService.getQuiz(quizId), HttpStatus.OK);
     }
 
-    @GetMapping(path = "get/{id}")
+    @GetMapping(path = "get/{id}", produces = "application/json")
     @Operation(summary = "Returns questions for quiz", description = "Provides a list of QuestionWrapper objects for the quiz", responses = {@ApiResponse(responseCode = "200", description = "status OK")})
     public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable("id") Integer id) {
         return new ResponseEntity<>(quizService.getQuestionForQuiz(id), HttpStatus.OK);
 
     }
 
-    @PostMapping("submit/{quizId}")
+    @PostMapping(path = "submit/{quizId}", consumes = "application/json")
     @Operation(summary = "Submits answers and returns a score", description = "Sends a list of QuizAnswers objects to question-service API and obtains a score", responses = {
             @ApiResponse(responseCode = "200", description = "status OK")})
     public ResponseEntity<Integer> submitAnswers(@PathVariable("quizId") Integer quizId, @RequestBody List<QuizAnswers> answersList) {
         return new ResponseEntity<>(quizService.getScore(quizId, answersList), HttpStatus.OK);
     }
 
-    @DeleteMapping("{quizId}")
+    @DeleteMapping(path = "{quizId}")
     @Operation(summary = "Deletes a quiz by ID", description = "Deletes quiz from database", responses = {@ApiResponse(responseCode = "200", description = "status OK")})
     public ResponseEntity<String> deleteQuiz(@PathVariable("quizId") Integer quizId) {
         return new ResponseEntity<>(quizService.deleteQuiz(quizId), HttpStatus.OK);
