b43a3c9ac1ad81e547347da0c9c5d250382a7510
Alejandro Casiva <alec667@gmail.com>
2024-01-25 23:57:51 -0300

    docs(QuizController): add documentation for getQuizQuestions(), submitAnswers() and deleteQuiz() methods

--- src/main/java/com/example/quizservice/controller/QuizController.java
+++ src/main/java/com/example/quizservice/controller/QuizController.java
@@ -34,17 +34,21 @@ public class QuizController {
     }
 
     @GetMapping(path = "get/{id}")
+    @Operation(summary = "Returns questions for quiz", description = "Provides a list of QuestionWrapper objects for the quiz", responses = {@ApiResponse(responseCode = "200", description = "status OK")})
     public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable("id") Integer id) {
         return new ResponseEntity<>(quizService.getQuestionForQuiz(id), HttpStatus.OK);
 
     }
 
     @PostMapping("submit/{quizId}")
+    @Operation(summary = "Submits answers and returns a score", description = "Sends a list of QuizAnswers objects to question-service API and obtains a score", responses = {
+            @ApiResponse(responseCode = "200", description = "status OK")})
     public ResponseEntity<Integer> submitAnswers(@PathVariable("quizId") Integer quizId, @RequestBody List<QuizAnswers> answersList) {
         return new ResponseEntity<>(quizService.getScore(quizId, answersList), HttpStatus.OK);
     }
 
     @DeleteMapping("{quizId}")
+    @Operation(summary = "Deletes a quiz by ID", description = "Deletes quiz from database", responses = {@ApiResponse(responseCode = "200", description = "status OK")})
     public ResponseEntity<String> deleteQuiz(@PathVariable("quizId") Integer quizId) {
         return new ResponseEntity<>(quizService.deleteQuiz(quizId), HttpStatus.OK);
     }
